8bc9738efbee4194972ac4ba77eefe9f80c08b3c
Peppe289 <gsperanza204@protonmail.com>
2024-02-19 16:07:44 +0100

    init pt. 2
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- jni/main.c
+++ jni/main.c
@@ -4,11 +4,20 @@
 
 struct task_simply_struct {
     pid_t pid;
-    char comm[200];
+    char comm[16];
 };
 
+struct task_simply_struct *task_list(long int *size) {
+    struct task_simply_struct *data;
+    const long int store_size = 2000;
 
+    data = malloc(store_size * sizeof(struct task_simply_struct));
 
-int someData() {
-    return syscall(445);
+    if ((*size = syscall(445, data, store_size)) < 0) {
+        perror("Syscall error");
+        free(data);
+        return NULL;
+    }
+
+    return data;
 }
