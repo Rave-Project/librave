8c08fe1ca792e7c088aea1af0bffd4e6a0d8664f
Peppe289 <gsperanza204@protonmail.com>
2024-03-03 18:42:27 +0100

    chose full path insead name
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- jni/main.c
+++ jni/main.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * @pid: process id
+ * @name: name of process with full path
+ * @rss_memory: resident memory size used
+ * @vm: virtual memory
+ */
 struct task_simply_struct {
     pid_t pid;
-    char comm[16];
+    char name[255];
+    unsigned long int rss_memory;
+    unsigned long int vm;
 };
 
 struct task_simply_struct *task_list(long int *size) {
