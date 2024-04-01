c8a5d3da482cc80b902a5f814a8ea16e6d852a82
Peppe289 <gsperanza204@protonmail.com>
2024-04-01 16:13:48 +0200

    add gpu syscall
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- jni/main.c
+++ jni/main.c
@@ -29,3 +29,19 @@ struct task_simply_struct *task_list(long int *size) {
 
     return data;
 }
+
+int gpu_getconf() {
+    unsigned int freq[10] = {0};
+    int i = 0;
+    char buff[20];
+    syscall(446, 0, freq, 0);
+    int max = 0;
+
+    for (; i < 10; ++i) {
+        if (max < freq[i]) {
+            max = freq[i];
+        }
+    }
+
+    return max;
+}
